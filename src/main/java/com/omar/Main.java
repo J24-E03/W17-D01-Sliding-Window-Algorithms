667e52acb71c2fedaec17f28dcf6bf802bfb635e
omarakamal <omaraakamal@gmail.com>
2025-02-03 11:42:51 +0300

    updated code

---
+++ src/main/java/com/omar/Main.java
@@ -0,0 +1,14 @@
+package com.omar;
+
+public class Main {
+
+//    given an array of values nums return the sub array with the largest value of size k
+    public static void main(String[] args) {
+
+    }
+
+    public static int maxSumSubArray(int[] nums, int k){
+        
+
+    }
+}
