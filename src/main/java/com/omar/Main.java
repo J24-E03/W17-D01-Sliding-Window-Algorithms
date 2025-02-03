4ea60535c9a1747383c44a1f10881cf644eae594
omarakamal <omaraakamal@gmail.com>
2025-02-03 12:56:06 +0300

    updated code

--- src/main/java/com/omar/Main.java
+++ src/main/java/com/omar/Main.java
@@ -5,10 +5,37 @@ public class Main {
 //    given an array of values nums return the sub array with the largest value of size k
     public static void main(String[] args) {
 
+
+        System.out.println(maxSumSubArray(new int[]{4,2,2,7,8,1,2,7,10},3));
+
+
     }
 
     public static int maxSumSubArray(int[] nums, int k){
-        
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
 
+        for(int i =0; i<nums.length; i++){
+
+//            populate the window with the next value in the array
+//            on each iteration
+            currentSum += nums[i];
+
+            if(i >=k-1){
+
+//                checks if the currentSum of our current window is
+//                bigger than the maxSum
+                maxSum = Math.max(currentSum,maxSum);
+
+//                taking out the first value in the window
+                currentSum -= nums[i - (k-1)];
+
+            }
+
+
+
+
+        }
+        return maxSum;
     }
 }
---
+++ src/main/java/com/omar/homework/CodeAlong.java
@@ -0,0 +1,49 @@
+package com.omar.homework;
+
+public class CodeAlong {
+
+    public static int searchInsertIndex(int[] nums, int target){
+        int left = 0;
+        int right = nums.length -1;
+
+        while(left < right){
+            int mid = left + (right -left)/2;
+
+//            midpoint is equal to target
+            if(nums[mid] == target) return mid;
+//            midpoint is less than the target
+            else if(nums[mid] < target) left = mid +1;
+            else if(nums[mid] > target) right = mid -1;
+        }
+
+//        if we dont find the number
+        return left;
+
+    }
+
+    public static int findPeakInArray(int[] nums){
+        int left = 0;
+        int right = nums.length -1;
+
+        while(left < right){
+            int mid =  left + (right - left)/2;
+
+            if(nums[mid] > nums[mid -1]){
+                right = mid;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return left;
+
+    }
+    public static void main(String[] args) {
+
+        int[] nums = {1,2,3,4,6,7,8};
+
+        System.out.println(searchInsertIndex(nums,5));
+//        System.out.println(findPeakInArray(nums));
+
+    }
+}
